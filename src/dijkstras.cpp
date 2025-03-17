089d28dedeb7dc307ed2f112cef337029f2961a5
Nathan Nguyen <nathapn2@uci.edu>
2025-03-16 19:23:03 -0700

    Removed code that broke autograder in main()

--- src/dijkstras.cpp
+++ src/dijkstras.cpp
@@ -63,19 +63,3 @@ void print_path(const vector<int> &v, int total) {
     cout << endl;
     cout << "Total cost is " << total << endl;
 }
-
-
-// int main() {
-//     Graph G;
-//     file_to_graph("src/largest.txt", G);
-
-//     vector<int> previous(G.size(), UNDEFINED);
-//     vector<int> distances = dijkstra_shortest_path(G, 0, previous);
-
-//     for (int i = 0; i < distances.size(); ++i) {
-//         vector<int> path = extract_shortest_path(distances, previous, i);
-//         print_path(path, distances[i]);
-//     }
-
-//     return 0; 
-// }
